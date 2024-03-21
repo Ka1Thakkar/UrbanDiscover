1ae9583e3a1b8b49946beb8098f6e07826cbf3f3
Kavan Thakkar <95115101+Ka1Thakkar@users.noreply.github.com>
2024-03-22 02:22:49 +0530

    feat : get geolocation of entered place

--- app/page.tsx
+++ app/page.tsx
@@ -2,15 +2,30 @@
 import Map from "@/components/map";
 import { Input } from "@/components/ui/input";
 import Image from "next/image";
-import { useEffect, useState } from "react";
+import { stringify } from "querystring";
+import { useEffect, useState, useLayoutEffect } from "react";
 
 export default function Home() {
   const [coordinates, setCoordinates] = useState<any>([])
-  useEffect(() => {
+  const [fetched, setFetched] = useState(false)
+  const [Data, setData] = useState<any>([])
+  const [value, setValue] = useState<any>([])
+
+  // const fetchMenu = async ({place} : any) => {
+  //   console.log(place)
+  //   const res = await fetch(`https://geocode.search.hereapi.com/v1/geocode?q=` + place+ `&apiKey=Y2rF_WsjbbUAFLC8WHMQGQIP-WkR_BYXZn8Jl44cJSc`);
+  //   const data = await res.json();
+  //   setSearchedCoordinates(data);
+  //   setFetching(true);
+  //   console.log(res, data, searchedCoordinates, fetching)
+  // };
+
+  const getGeoCode = () => {}
+
+  useLayoutEffect(() => {
     const options = {
       enableHighAccuracy: true,
       maximumAge: 0,
-      timeout : 5000
     };
 
     function success(pos: { coords: any; }) {
@@ -24,10 +39,35 @@ export default function Home() {
     }
     
     navigator.geolocation.getCurrentPosition(success, error, options);
-  }, [0])
+
+  }, [setCoordinates])
+
+  useEffect(() => {
+    document.getElementById('input')?.addEventListener('keydown', async (e) => {
+      if (e.key === 'Enter') {
+        const place = (document.getElementById('input') as HTMLInputElement)?.value.toString();
+        console.log(place)
+        
+        const res = await fetch(`https://geocode.search.hereapi.com/v1/geocode?q=` + place + `&apiKey=Y2rF_WsjbbUAFLC8WHMQGQIP-WkR_BYXZn8Jl44cJSc`);
+        const data = await res.json();
+        const lat = await data.items[0].position.lat;
+        const lng = await data.items[0].position.lng;
+        setFetched(true)
+        setData(data)
+        console.log(data, lat, lng)
+      }
+      else{
+        setValue((document.getElementById('input') as HTMLInputElement)?.value)
+      }
+    })
+    if (fetched) {
+      setCoordinates([Data.items[0].position.lat, Data.items[0].position.lng])
+      console.log(coordinates)
+    }
+  },[fetched])
   return (
-    <main className="relative max-w-screen">
-      <Input className="w-[30vw] rounded-full bg-[#5E5E5E]/75 border-none text-xl font-bold backdrop-blur-md absolute z-[99] top-5 left-5" />
+    <main className="relative max-w-screen text-white">
+      <Input id="input" className="w-[30vw] rounded-full bg-[#5E5E5E]/75 border-none text-xl font-bold backdrop-blur-md absolute z-[99] top-5 left-5"/>
       <Map coordinates={coordinates} />
     </main>
   );
