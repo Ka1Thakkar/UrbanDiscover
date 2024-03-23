19b32d00da29920e362c9c114c9386df8443ec39
Kavan Thakkar <95115101+Ka1Thakkar@users.noreply.github.com>
2024-03-23 10:47:16 +0530

    feat : markers read more modal added + Multiple types of tourist places now fetched through the api.

--- app/page.tsx
+++ app/page.tsx
@@ -2,12 +2,14 @@
 import { Input } from "@/components/ui/input";
 import { Search } from "lucide-react";
 import { useEffect, useLayoutEffect, useState, } from "react";
-import { Roboto } from "next/font/google";
+import { DM_Sans } from "next/font/google";
 import dynamic from "next/dynamic";
+import { DropdownMenu, DropdownMenuContent, DropdownMenuItem, DropdownMenuLabel, DropdownMenuSeparator, DropdownMenuTrigger } from "@/components/ui/dropdown-menu";
+import { cn } from "@/lib/utils";
 
 const Map = dynamic(() => import('@/components/map'), { ssr: false });
 
-const mainFont = Roboto({ subsets: ['latin'], weight: ['400'] });
+const mainFont = DM_Sans({ subsets: ['latin'], weight: ['400'] });
 
 export default function Home() {
   const [coordinates, setCoordinates] = useState<any>([])
@@ -15,6 +17,8 @@ export default function Home() {
   const [Data, setData] = useState<any>([])
   const [value, setValue] = useState<any>([])
   const [suggestData, setSuggestData] = useState<any>([]);
+  const [layerType, setLayerType] = useState<any>('OpenStreetMap')
+  const [layerMode, setLayerMode] = useState<any>('dark')
 
   useLayoutEffect(() => {
     const options = {
@@ -41,7 +45,7 @@ export default function Home() {
       if (e.key === 'Enter') {
         const place = (document.getElementById('input') as HTMLInputElement)?.value.toString();
 
-        const res = await fetch(`https://geocode.search.hereapi.com/v1/geocode?q=` + place + `&apiKey=I8lpkuRyJfxK5An9NzPrFalvL_9Nyjh5Qx76yN9xRQs`);
+        const res = await fetch(`https://geocode.search.hereapi.com/v1/geocode?q=` + place + `&apiKey=oJawmOVpYcJE0LK4Gr1nHMJjZDVPgS5CJ7i-ThpuxKA`);
         const data = await res.json();
         const lat = await data.items[0].position.lat;
         const lng = await data.items[0].position.lng;
@@ -57,7 +61,7 @@ export default function Home() {
 
   useEffect(() => {
     const getSuggestions = (async () => {
-      const res = await fetch(`https://autosuggest.search.hereapi.com/v1/autosuggest?at=` + coordinates[0] + `,` + coordinates[1] + `&limit=5&lang=en&q=` + value + `&apiKey=I8lpkuRyJfxK5An9NzPrFalvL_9Nyjh5Qx76yN9xRQs`)
+      const res = await fetch(`https://autosuggest.search.hereapi.com/v1/autosuggest?at=` + coordinates[0] + `,` + coordinates[1] + `&limit=5&lang=en&q=` + value + `&apiKey=oJawmOVpYcJE0LK4Gr1nHMJjZDVPgS5CJ7i-ThpuxKA`)
       const data = await res.json();
       const filteredData = data.items.filter((item: any) => item.resultType === "administrativeArea" || item.resultType === 'locality');
       setSuggestData(filteredData)
@@ -66,6 +70,8 @@ export default function Home() {
 
     if (value.length !== 0) {
       getSuggestions();
+    } else {
+      setSuggestData([])
     }
 
   }, [value])
@@ -76,35 +82,69 @@ export default function Home() {
     setCoordinates([data.position.lat,data.position.lng]);
   }
 
+  const onLayerClick = (layer : string, mode : string) => {
+    setLayerType(layer)
+    setLayerMode(mode)
+  }
+
   return (
     <main className={mainFont.className + " relative max-w-screen text-gray-900"}>
-      <div className="w-[30vw] rounded-full bg-gray-300/60 border-none text-xl font-semibold backdrop-blur absolute z-[99999] top-5 left-5 overflow-hidden flex gap-5 items-center pl-2 pr-5">
-        <Input id="input" className="bg-transparent border-none rounded-full text-xl" onChange={e => setValue(e.target.value)} value={value} />
-        <Search />
-      </div>
-      {suggestData.length !== 0 && (
-        <div className="absolute top-20 z-[99999] flex flex-col gap-2 left-5 bg-gray-300/60 backdrop-blur w-[30vw] rounded-xl p-5">
-          {suggestData.map((data: any, id: number) => {
-            return (
-              <div key={id} role="button" onClick={e => changeTheMap(data)}>
-                <h1 className="text-xl font-medium">
-                  {data.title}
-                </h1>
-                <p className="flex gap-2">
-                  <span>
-                    {data.resultType === "administrativeArea" && data.administrativeAreaType}
-                    {data.resultType === "locality" && data.localityType}
-                  </span>
-                  <span>
-                    ({data.position.lat}, {data.position.lng})
-                  </span>
-                </p>
-              </div>
-            )
-          })}
+      <div className="absolute z-[9999] top-5 left-5 flex gap-5 items-center">
+        <div className="w-[30vw] rounded-full bg-gray-300/60 border-none text-xl font-semibold backdrop-blur overflow-hidden flex gap-5 items-center pl-2 pr-5">
+          <Input id="input" className="bg-transparent border-none rounded-full text-xl" onChange={e => setValue(e.target.value)} value={value} />
+          <Search />
         </div>
-      )}
-      <Map coordinates={coordinates}/>
+        <div className="rounded-full bg-gray-300/60 text-xl font-semibold backdrop-blur px-5 py-2">
+          <DropdownMenu>
+            <DropdownMenuTrigger className="z-[9999] border-none outline-none">Layers</DropdownMenuTrigger>
+            <DropdownMenuContent className="z-[9999] bg-gray-300/60 backdrop-blur mt-5 ml-24">
+              <DropdownMenuLabel>Choose the layer you prefer:</DropdownMenuLabel>
+              <DropdownMenuSeparator />
+              <DropdownMenuItem>
+                <button onClick={() => onLayerClick('OpenStreetMap', 'light')}>
+                  OpenStreetMap Light
+                </button>
+              </DropdownMenuItem>
+              <DropdownMenuItem>
+                <button onClick={() => onLayerClick('OpenStreetMap', 'dark')}>
+                  OpenStreetMap Dark
+                </button>
+              </DropdownMenuItem>
+              <DropdownMenuItem>
+                <button onClick={() => onLayerClick('HERE', 'light')}>
+                  HERE Maps Light
+                </button>
+              </DropdownMenuItem>
+              <DropdownMenuItem>
+                <button onClick={() => onLayerClick('HERE', 'dark')}>
+                  HERE Maps Dark
+                </button>
+              </DropdownMenuItem>
+            </DropdownMenuContent>
+          </DropdownMenu>
+        </div>
+      </div>
+      <div className={cn(suggestData.length !== 0  ? "absolute top-20 z-[99999] flex flex-col gap-2 left-5 bg-gray-300/60 backdrop-blur w-[30vw] rounded-xl p-5" : 'hidden')}>
+        {suggestData.map((data: any, id: number) => {
+          return (
+            <div key={id} role="button" onClick={e => changeTheMap(data)}>
+              <h1 className="text-xl font-medium">
+                {data.title}
+              </h1>
+              <p className="flex gap-2">
+                <span>
+                  {data.resultType === "administrativeArea" && data.administrativeAreaType}
+                  {data.resultType === "locality" && data.localityType}
+                </span>
+                <span>
+                  ({data.position.lat}, {data.position.lng})
+                </span>
+              </p>
+            </div>
+          )
+        })}
+      </div>
+      <Map coordinates={coordinates} layer={layerType} mode={layerMode}/>
     </main>
   );
 }
--- components/map.tsx
+++ components/map.tsx
@@ -1,38 +1,47 @@
 import { useEffect, useState } from "react";
-import { HPlatform, HMap } from "react-here-map";
 import 'leaflet/dist/leaflet.css';
 import { MapContainer, TileLayer, Marker, Popup, useMapEvents, useMap } from 'react-leaflet';
-
+import markerIcon from '@/public/marker.svg'
 import L from 'leaflet';
+import { CircleX } from "lucide-react";
+import { DM_Serif_Display, DM_Sans } from "next/font/google";
+import { cn } from "@/lib/utils";
 
-const myIcon = new L.Icon({
-    iconUrl: require('../public/marker.svg'),
-    iconRetinaUrl: require('../public/marker.svg'),
-    popupAnchor:  [-0, -0],
-    iconSize: new L.Point(50,50), 
-    shadowUrl: undefined,    
-    shadowSize : [0,0],
-    shadowRetinaUrl: undefined,
-    className: 'leaflet-div-icon'
+const headingFont = DM_Serif_Display({ subsets: ['latin'], weight: ['400'] });
+
+const myIcon = L.divIcon({
+    html: `<img src=${markerIcon.src} alt="marker" />`,
+    iconSize: [0, 0],
+    className: 'leaflet-div-icon',
+    iconAnchor: [-15, -15]
 });
 
+
 interface MapProps {
-    coordinates: number[]
+    coordinates: number[],
+    layer : string,
+    mode : string,
 }
 
-const Map = ({ coordinates }: MapProps) => {
+const Map = ({ coordinates, layer, mode }: MapProps) => {
         const lat = coordinates[0]
         const lng = coordinates[1]
         const [markers, setMarkers] = useState<any>([])
         const [Data, setData] = useState<any>([])
+        const [modal, setModal] = useState(false)
+        const [markerData, setMarkerData] = useState<any>([])
 
         useEffect(() => {
             const getMarkers = (async () => {
                 console.log("inside getSuggestions");
-                const res = await fetch(`https://discover.search.hereapi.com/v1/discover?in=circle:`+ coordinates[0] + `,` + coordinates[1] + `;r=20000&q=historical+monuments&apiKey=I8lpkuRyJfxK5An9NzPrFalvL_9Nyjh5Qx76yN9xRQs`)
-                const data = await res.json();
+                const resHistoricalMonuments = await fetch(`https://discover.search.hereapi.com/v1/discover?in=circle:`+ coordinates[0] + `,` + coordinates[1] + `;r=200000&q=historical+monuments&apiKey=v3oh-ib_9QUwuc4GFdDzm8I21nq41fVaTyjro6eXQE0`)
+                const resTouristAttractions = await fetch(`https://discover.search.hereapi.com/v1/discover?in=circle:`+ coordinates[0] + `,` + coordinates[1] + `;r=2000000&q=tourist+attractions&apiKey=v3oh-ib_9QUwuc4GFdDzm8I21nq41fVaTyjro6eXQE0`)
+                const resLandmarkAttractions = await fetch(`https://discover.search.hereapi.com/v1/discover?in=circle:`+ coordinates[0] + `,` + coordinates[1] + `;r=2000000&q=landmark+attractions&apiKey=v3oh-ib_9QUwuc4GFdDzm8I21nq41fVaTyjro6eXQE0`)
+                const dataHistoricalMonuments = await resHistoricalMonuments.json();
+                const dataTouristAttractions = await resTouristAttractions.json();
+                const dataLandmarkAttractions = await resLandmarkAttractions.json();
+                const data = dataHistoricalMonuments.items.concat(dataTouristAttractions.items, dataLandmarkAttractions.items)
                 setMarkers(data)
-                console.log(Data)
               }
             );
             if(coordinates[0]!==undefined && coordinates[1]!==undefined){
@@ -41,6 +50,8 @@ const Map = ({ coordinates }: MapProps) => {
       
         }, [coordinates])
         return (
+            <>
+            {modal && (<Modal markerData={markerData} state={modal} stateFunction={setModal} />)}
             <MapContainer
                 id="Map"
                 center={[28.679079, 77.069710]}
@@ -50,36 +61,45 @@ const Map = ({ coordinates }: MapProps) => {
                 zoomAnimation
                 className="w-[100vw] h-[100vh]"
             >
-                {/* {() => {
-                    const L = require('leaflet');
-                    const myIcon = new L.Icon({
-                        iconUrl: require('../public/marker.svg'),
-                        iconRetinaUrl: require('../public/marker.svg'),
-                        popupAnchor:  [-0, -0],
-                        iconSize: new L.Point(50,50), 
-                        shadowUrl: undefined,    
-                        shadowSize : [0,0],
-                        shadowRetinaUrl: undefined,
-                        className: 'leaflet-div-icon'
-                    });
-                    return (myIcon)
-                }} */}
-                <TileLayer
+                {layer === 'HERE' && mode === 'light' &&(<TileLayer
+                    url="https://2.base.maps.ls.hereapi.com/maptile/2.1/maptile/newest/normal.day/{z}/{x}/{y}/512/png8?apiKey=I8lpkuRyJfxK5An9NzPrFalvL_9Nyjh5Qx76yN9xRQs&ppi=320"
+                    // attribution="&copy; <a>HERE Maps</a> contributors" 
+                    className=" grayscale"
+                />)}
+                {layer === 'HERE' && mode === 'dark' &&(<TileLayer
+                    url="https://2.base.maps.ls.hereapi.com/maptile/2.1/maptile/newest/reduced.night/{z}/{x}/{y}/512/png8?apiKey=I8lpkuRyJfxK5An9NzPrFalvL_9Nyjh5Qx76yN9xRQs&ppi=320"
+                    // attribution="&copy; <a>HERE Maps</a> contributors" 
+                    className=" grayscale contrast-200"
+                />)}
+                {layer === 'OpenStreetMap' && mode === 'light' &&(<TileLayer
                     url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
-                    attribution="&copy; <a href=&quot;http://osm.org/copyright&quot;>OpenStreetMap</a> contributors" 
-                    className=" grayscale invert <brightness-75></brightness-75> contrast-100"
-                />
+                    // attribution="&copy; <a>OpenStreetMap</a> contributors" 
+                    className=" grayscale"
+                />)}
+                {layer === 'OpenStreetMap' && mode === 'dark' &&(<TileLayer
+                    url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+                    // attribution="&copy; <a>OpenStreetMap</a> contributors" 
+                    className=" grayscale invert"
+                />)}
                 <RecenterAutomatically lat={lat} lng={lng} />
-                {markers.items?.map((marker: any, index : any) => {
+                {markers?.map((marker: any, index : any) => {
                     return (
                         <Marker icon={myIcon} key={index} position={[marker.position.lat, marker.position.lng]}>
-                            <Popup>
-                                {marker.title}
+                            <Popup className="">
+                                <div className="py-2 px-5">
+                                    <h1 className=" text-base font-semibold">
+                                        {marker.title}
+                                    </h1>
+                                    <button className=" text-blue-950 pt-2" onClick={() => {setModal(true); setMarkerData(marker)}}>
+                                        Read More...
+                                    </button>
+                                </div>
                             </Popup>
                         </Marker>
                     )
                 })}
             </MapContainer>
+            </>
     );
 }
 
@@ -88,7 +108,35 @@ export default Map;
 const RecenterAutomatically = ({lat,lng} : any) => {
     const map = useMap();
      useEffect(() => {
-       map.setView([lat, lng]);
+       if (lat !== undefined )
+        {
+            map.setView([lat, lng]);
+        } else {
+            map.setView([28.679079, 77.069710]);
+        }
     }, [lat, lng]);
     return null;
+}
+
+interface ModalProps {
+    markerData : any,
+    state : boolean,
+    stateFunction : any
+}
+
+const Modal = ({markerData, state, stateFunction} : ModalProps) => {
+    return (
+        <div className={cn("absolute z-[99999] w-screen h-screen top-0 left-0 flex items-center justify-center", headingFont.className)}>
+            <div className="w-[75vw] h-[60vh] bg-gray-300 backdrop-blur p-5 rounded-xl">
+                <div role="button" onClick={() => stateFunction(!state)}>
+                    <CircleX />
+                </div>
+                <div className="pt-10">
+                    <h1 className="text-5xl uppercase">
+                        {markerData.title}
+                    </h1>
+                </div>
+            </div>
+        </div>
+    )
 }
